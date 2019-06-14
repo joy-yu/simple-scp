90e73f6dc1241d79a4be25d123b348ba190761a8
joy-yu <775154773@qq.com>
2019-06-14 14:31:05 +0800

    fix directly usage of scp2

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const client = require('scp2');
 const colors = require('colors');
 
-module.exports = class SSH {
-  static scp2 = client;
-
+class SSH {
   constructor(options = {}) {
     this.startTime = null;
     this.options = options;
+    this.scp2 = client;
   }
 
   dest(local, remote) {
     let startDate = Date.now();
     let opts = { ...this.options, path: remote };
     console.log(colors.rainbow('\nStart upload, please wait...'));
-    client.scp(local, opts, err => {
+    this.scp2.scp(local, opts, err => {
       if (err) throw err;
       console.log(colors.green(`Uploaded ${colors.red(local)} successfully in ${Date.now() - startDate}ms`));
     });
   }
-};
+}
+
+module.exports = SSH;
